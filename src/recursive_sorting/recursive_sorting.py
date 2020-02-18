15524cd4ccb6ab59b7d5c20456e00a2e5d014794
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-02-18 11:09:44 -0800

    wrote quicksort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,16 +1,20 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
     
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-
+    if len(arr) == 1:
+        return arr
+    else:
+        merge_sort(arr[0:len(arr)//2]) + merge_sort(arr[len(arr)//2::])
     return arr
 
 
